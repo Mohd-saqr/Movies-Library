20a54d12f373d48dc6624b3046b3604160843424
msqer998 <saqerabu9@gmail.com>
2022-04-11 00:08:28 +0300

    edite schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,16 @@
-DROP TABLE IF EXISTS famovis;
-CREATE TABLE IF NOT EXISTS famovis(
+-- DROP TABLE IF EXISTS famovis;
+-- CREATE TABLE IF NOT EXISTS famovis(
+--     id SERIAL PRIMARY KEY,
+--     title text,
+--     overview text,
+--     imgurl varchar(512),
+--     release_date varchar(256),
+--     rate float , 
+--     lang varchar(256),
+--     vot_count float
+-- );
+DROP TABLE IF EXISTS trending;
+CREATE TABLE IF NOT EXISTS trending(
     id SERIAL PRIMARY KEY,
     title text,
     overview text,
