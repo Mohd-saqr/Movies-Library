6992f9f5adbe5987940ff574eef1b674f4a01ebb
msqer998 <saqerabu9@gmail.com>
2022-04-10 23:52:23 +0300

    edite schema

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 require(`dotenv`).config()
 const pg =require(`pg`)
 const path = require(`path`)
-// const client = new pg.Client({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: { rejectUnauthorized: false }
-// });
+const client = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 
-const client = new pg.Client(process.env.DATABASE_URL)
+// const client = new pg.Client(process.env.DATABASE_URL)
 
 
 const express = require(`express`)
